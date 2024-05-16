dd05b642bbc8251319a01e21abdde6023e7bd39f
Madarász Tamás <m.tamas1012@gmail.com>
2024-05-16 18:22:36 +0200

    added login modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,27 +2,33 @@ import padlock from '../assets/icons/padlock.svg';
 import twitter from '../assets/icons/twitter.svg';
 import fb from '../assets/icons/fb.svg';
 import instagram from '../assets/icons/instagram.svg';
+import { useState } from 'react';
+import LoginModal from './LoginModal';
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-row justify-end space-x-2">
-        <button className="p-1 flex flex-row items-center">
-          {/* Does nothing */}
-          <img src={instagram} className='w-4 text-white' alt="Instagram Button" />
-        </button>
-        <button className="p-1 flex flex-row items-center">
-          {/* Does nothing */}
-          <img src={fb} className='w-4 text-white' alt="Facebook Button" />
-        </button>
-        <button className="p-1 flex flex-row items-center">
-          {/* Does nothing */}
-          <img src={twitter} className='w-4 text-white' alt="Twitter Button" />
-        </button>
-        <button className="bg-white px-3 py-1 flex flex-row items-center uppercase">
-          {/* TODO: Add login modal */}
-          <img src={padlock} className='w-4' alt="Login Button" />
-          Login
-        </button>
-      </div>
+    <>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-row justify-end space-x-2">
+      <button className="p-1 flex flex-row items-center">
+        {/* Does nothing */}
+        <img src={instagram} className='w-4 text-white' alt="Instagram Button" />
+      </button>
+      <button className="p-1 flex flex-row items-center">
+        {/* Does nothing */}
+        <img src={fb} className='w-4 text-white' alt="Facebook Button" />
+      </button>
+      <button className="p-1 flex flex-row items-center">
+        {/* Does nothing */}
+        <img src={twitter} className='w-4 text-white' alt="Twitter Button" />
+      </button>
+      <button className="bg-white px-3 py-1 flex flex-row items-center uppercase" onClick={() => setShowLogin(!showLogin)}>
+        <img src={padlock} className='w-4' alt="Login Button"/>
+        Login
+      </button>
+    </div>
+    <LoginModal show={showLogin} onClose={() => setShowLogin(false)} />
+    </>
   )
 }
---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+type Props = {
+  show: boolean;
+  onClose: () => void;
+};
+
+export default function LoginModal({ show, onClose }: Props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  
+  if (!show) {
+    return null;
+  }
+
+  function fakeAuth(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!username || !password) {
+      setError('Username and password are required');
+      return;
+    }
+
+    if (username === 'admin' && password === 'admin') {
+      onClose();
+    } else {
+      setError('Invalid username or password');
+    }
+  }
+
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50" onClick={onClose}>
+      <div className="bg-white p-6 rounded-lg max-w-md w-full mx-auto overflow-auto" onClick={e => e.stopPropagation()}>
+      <form onSubmit={fakeAuth}>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 border border-gray-300 rounded mb-4" />
+        <input type="password"  placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-2 border border-gray-300 rounded mb-4" />
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+        <button type="submit" className="w-full p-2 bg-orange-500 text-white rounded">Login</button>
+      </form>
+      </div>
+    </div>
+  );
+
+}
